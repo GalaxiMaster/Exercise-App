6d336519785eaaa95dee6dff8737e681f6907ce6
Dylan <dmj08bot@gmail.com>
2025-01-03 20:05:33 +1100

    Added firebase in the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:exercise_app/encryption_controller.dart';
+import 'package:exercise_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Pages/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -10,6 +17,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    addUser();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // theme: ThemeData(fontFamily: 'Poppins'),
